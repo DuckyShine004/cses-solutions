782745b95f5eec5bdcf82abc39dfaed94b99a27a
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-16 01:48:13 +1300

    feat: Added Solution To Weird Algorithm

---
+++ introductory-problems/weird-algorithm.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	long long n;
+	cin >> n;
+
+	while (n > 1) {
+		cout << n << " ";
+		n = (n & 1) ? (3 * n) + 1 : n / 2;
+	}
+
+	cout << 1;
+}
