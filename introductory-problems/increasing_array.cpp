b2044e987ea1c003c1cd086b7b7e5322f202c958
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-16 02:12:17 +1300

    feat: Added Solution To Increasing Array

---
+++ introductory-problems/increasing_array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, val;
+    cin >> n;
+
+    long long res = 0;
+
+    vector<int> V(n);
+    
+    for (int &v : V) {
+        cin >> v;
+    }
+
+    for (int i = 1; i < n; i++) {
+        if (V[i - 1] > V[i]) {
+            val = V[i - 1] - V[i];
+
+            V[i] += val;
+            res += val;
+        }
+    }
+
+    cout << res;
+}
