a189178e0807c16ecae716c22f01f8804e6c3298
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-16 01:52:31 +1300

    feat: Added Solution To Missing Number

---
+++ introductory-problems/missing_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, idx;
+	cin >> n;
+
+	vector<int> V(n, false);
+	
+	for (int i = 0; i < n - 1; i++) {
+		cin >> idx;
+		V[idx - 1] = true;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (!V[i]) {
+			cout << i + 1;
+		}
+	}
+}
